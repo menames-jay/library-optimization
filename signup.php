08c4aea35da03cdd1ea23ca1fbc9eab816306fdf
Jayanth K <118156798+menames-jay@users.noreply.github.com>
2024-02-01 19:50:14 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOW</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+</head>
+<style type="text/css">
+	#main_content{
+		padding: 50px;
+		background-color: whitesmoke;
+	}
+	#side_bar{
+		background-color: whitesmoke;
+		padding: 50px;
+		width: 300px;
+		height: 450px;
+	}
+</style>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">Library Optimization Website</a>
+			</div>
+	
+		    <ul class="nav navbar-nav navbar-right">
+		      <li class="nav-item">
+		        <a class="nav-link" href="admin/index.php">Admin Login</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="signup.php"></span>Register</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="index.php">Student Login</a>
+		      </li>
+		    </ul>
+		</div>
+	</nav><br>
+	<span><marquee>Welcome to our Library Optimization Website. Unfortunately, we are still in the prototyping stage of our project, so please come back later.</marquee></span><br><br>
+	<div class="row">
+		<div class="col-md-4" id="side_bar">
+			<h5>Library Timings</h5>
+			<ul>
+				<li>9am to 4pm</li>
+			</ul>
+			<h5>Library Facilities</h5>
+			<ul>
+				<li>Extensive Collection</li>
+				<li>News Papers and Journals</li>
+				<li>Peaceful Environment</li>
+			</ul>
+		</div>
+		<div class="col-md-8" id="main_content">
+			<center><h3><u>User Registration Form</u></h3></center>
+			<form action="register.php" method="post">
+				<div class="form-group">
+					<label for="name">Full Name:</label>
+					<input type="text" name="name" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="email">Email ID:</label>
+					<input type="text" name="email" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input type="password" name="password" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="mobile">Mobile:</label>
+					<input type="text" name="mobile" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="address">Address:</label>
+					<textarea name="address" class="form-control" required></textarea> 
+				</div>
+				<button type="submit" class="btn btn-primary">Register</button>	
+			</form>
+		</div>
+	</div>
+</body>
+</html>
